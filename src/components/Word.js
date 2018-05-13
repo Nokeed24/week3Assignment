6a40a60cb0015c4496daf2a996880faba4645da5
Nokeed24 <nokid_gr@hotmail.it>
2018-05-13 15:19:59 +0200

    made case insensitive, show word at loss and minor styling

--- src/components/Word.js
+++ src/components/Word.js
@@ -2,18 +2,29 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import './Word.css'
-import { showGuess } from '../lib/game'
+import { showGuess, gameFinished, isWinner } from '../lib/game'
 
 export class Word extends PureComponent {
   static propTypes = {
     word: PropTypes.string.isRequired,    
     letters: PropTypes.arrayOf(PropTypes.string).isRequired,
-    show: PropTypes.string.isRequired
+    show: PropTypes.string.isRequired,
+    finished: PropTypes.bool.isRequired,
+    winner: PropTypes.bool.isRequired 
   }
 
   render() {
-    const { show } = this.props
+    const { word ,show, finished, winner } = this.props 
     var wordArray = show
+    if(finished && !winner) 
+    { 
+      return ( 
+        <div className="Word"> 
+        <span>The word was: {word}</span>       
+       </div> 
+ 
+      ) 
+    } 
     return (
        <div className="Word">
         <span>{wordArray}</span>      
@@ -25,7 +36,9 @@ export class Word extends PureComponent {
 const mapStateToProps = ({ word, letters }) => ({
   word,
   letters,
-  show: showGuess(word,letters)
+  show: showGuess(word,letters), 
+  finished: gameFinished(word,letters),
+  winner: isWinner(word,letters) 
 })
 
 export default connect(mapStateToProps)(Word)
